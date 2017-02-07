91a76abfc554cc38ae29392df48e86cf5c55e63b
Matthew Graham <mwerevu@users.noreply.github.com>
2017-02-06 23:17:20 -0500

    Updating the test code with stat calculator.

--- test3.py
+++ test3.py
@@ -4,15 +4,35 @@
 # - Finish parameterizing the function with the range.
 
 # Import Packages
+from numpy.linalg import norm
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.stats import entropy
+from math import sqrt
 
 # Initialize some stuff
 loc, scale = 0., 100.
 t = []
 arraylen = 1000
 bincount = 29
+
+def kld(P,Q):
+    _P = np.array(P) / norm(P, ord=1)
+    _Q = np.array(Q) / norm(Q, ord=1)
+    return round(np.sum([v for v in _P * np.log2(_P/_Q) if (not np.isnan(v) and not np.isinf(v))]),12)
+
+def jsd(P,Q):
+    _P = np.array(P) / norm(P, ord=1)
+    _Q = np.array(Q) / norm(Q, ord=1)
+    M = 0.5 * (_P + _Q)
+    return round(0.5 * (kld(_P,M) + kld(_Q,M)),12)
+
+def rmse(P,Q):
+    _P = np.array(P)
+    _Q = np.array(Q)
+    return round(sqrt(np.mean([np.power(_P - _Q,2)])),12)
+
 
 def plotComp(pdist,qdist,bincount):
 
@@ -21,9 +41,18 @@ def plotComp(pdist,qdist,bincount):
     pcount, pbins = np.histogram(pdist,mybins)
     qcount, qbins = np.histogram(qdist,mybins)
 
+    #print(pcount)
+    #print(qcount)
+    jsd1 = jsd(pcount,qcount)
+    print("jsd = %f" % jsd1)
+    print("rjsd = %f" % sqrt(jsd1))
+    rmse1 = rmse(pcount,qcount)
+    print("rmse = %f" % rmse1)
+    print("normed rmse = %f" % (rmse1/arraylen))
+    
     # Settings for plot
     pos = list(range(len(pcount)))
-    width = 0.33
+    width = 0.25
 
     # Plotting the bars
     fig, ax = plt.subplots(figsize=(10,5))
@@ -47,6 +76,10 @@ def plotComp(pdist,qdist,bincount):
         color='#F78F1E',
         label="Q"
         )
+
+    # Set Title
+    stats = "Jensen-Shannon Divergence (JSD) = %f\n Root Mean Squared Error = %f" % (jsd1,rmse1)
+    ax.set_title(stats)
     
     # Setting the x-axis and y-axis limits
     plt.xlim(min(pos)-width, max(pos)+width*4)
@@ -58,7 +91,6 @@ def plotComp(pdist,qdist,bincount):
     plt.grid()
     plt.show()
 
-    
 
 
 
@@ -67,7 +99,7 @@ def plotComp(pdist,qdist,bincount):
 #psamp = pd.DataFrame(np.random.uniform(loc,scale,arraylen), columns = ['P'])
 #qsamp = pd.DataFrame(np.random.uniform(loc,scale,arraylen), columns = ['Q'])
 psamp = pd.DataFrame(np.random.normal(30,20,arraylen), columns = ['P'])
-qsamp = pd.DataFrame(np.random.normal(70,10,arraylen), columns = ['Q'])
+qsamp = pd.DataFrame(np.random.normal(40,10,arraylen), columns = ['Q'])
 
 
 
